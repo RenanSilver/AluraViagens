8faf83481b1eefa015ab507a6eb78768e961d325
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-10-20 02:42:41 -0300

    Build de dados e criação do Adapter/List

--- app/src/main/java/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package com.alura.aluraviagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.alura.aluraviagens.R;
+import com.alura.aluraviagens.dao.PacoteDAO;
+import com.alura.aluraviagens.model.Pacote;
+import com.alura.aluraviagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this));
     }
 }
