4e7b58535ef8924ce4ce258f5b6f0d6ad905e175
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-10-25 18:55:51 -0300

    Bildando" itens do pacote para ResumoPacoteActivity (local,imagem,dias,preço)

--- app/src/main/java/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/com/alura/aluraviagens/ui/activity/ResumoPacoteActivity.java
@@ -2,9 +2,19 @@ package com.alura.aluraviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alura.aluraviagens.R;
+import com.alura.aluraviagens.model.Pacote;
+import com.alura.aluraviagens.util.DiasUtil;
+import com.alura.aluraviagens.util.MoedaUtil;
+import com.alura.aluraviagens.util.ResourceUtil;
+
+import java.math.BigDecimal;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -12,5 +22,25 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp",
+                2, new BigDecimal("243.99"));
+
+        TextView local = findViewById(R.id.resumo_pacote_local);
+        local.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil
+                .devolveDrawable(this, pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView dias = findViewById(R.id.resumo_pacote_dias);
+        String diasemTexto = DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        dias.setText(diasemTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil
+                .formataParaBrasileiro(pacoteSaoPaulo.getPreco());
+        preco.setText(moedaBrasileira);
     }
 }
