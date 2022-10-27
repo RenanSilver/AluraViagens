ecff363568d79bc41d42cef1d046d1cdff86cf2f
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-10-27 19:06:10 -0300

    adicionando o arquivo da nova Activity_pagamento.java

---
+++ app/src/main/java/com/alura/aluraviagens/ui/activity/Activity_pagamento.java
@@ -0,0 +1,14 @@
+package com.alura.aluraviagens.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_pagamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento);
+    }
+}
