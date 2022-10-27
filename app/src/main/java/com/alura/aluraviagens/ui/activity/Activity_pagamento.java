59c17c1ef07f93a4e7e88ab3ac4673e22016c756
RenanSilver <101308960+RenanSilver@users.noreply.github.com>
2022-10-27 19:25:30 -0300

    inserindo textViews Activity_pagamento.java

--- app/src/main/java/com/alura/aluraviagens/ui/activity/Activity_pagamento.java
+++ app/src/main/java/com/alura/aluraviagens/ui/activity/Activity_pagamento.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.alura.aluraviagens.R;
+
 public class Activity_pagamento extends AppCompatActivity {
 
     @Override
